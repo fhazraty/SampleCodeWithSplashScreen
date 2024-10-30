9e430ce3ca2f0b8cb259583b0106e630f64d95ab
Farhad <f.hazraty@gmail.com>
2024-10-30 07:42:30 +0330

    اضافه کردن  SqlHelper و دریافت ConnectionString از ورودی Constructor

---
+++ DataAccessLayer/SqlHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer
+{
+	public class SqlHelper
+	{
+        public string ConnectionString { get; set; }
+        public SqlHelper(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+    }
+}
